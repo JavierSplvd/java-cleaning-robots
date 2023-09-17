2d090c65e2178198926e1eb4dd168b83b03d1458
javier <javier.sepulveda.verdugo@gmail.com>
2023-09-17 17:45:20 +0200

    Created CreateRobotStateTest.

---
+++ src/test/java/com/javier/usecases/CreateRobotStateTest.java
@@ -0,0 +1,35 @@
+package com.javier.usecases;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.Test;
+
+import com.javier.entities.FactoryFloorGrid;
+import com.javier.enums.Heading;
+
+public class CreateRobotStateTest {
+    @Test
+    public void whenInput_shouldReturnAssignMovementState() {
+        FactoryFloorGrid grid = FactoryFloorGrid.of("5 5");
+        CliState state = new CreateRobotState(grid);
+
+        state = state.input("2 2 W");
+
+        assert (state instanceof AssignMovementState);
+        assert (state.getGrid().getCleaningRobots().size() == 1);
+        assert (state.getGrid().getCleaningRobots().get(0).position().x() == 2);
+        assert (state.getGrid().getCleaningRobots().get(0).position().y() == 2);
+        assert (state.getGrid().getCleaningRobots().get(0).heading().equals(Heading.W));
+
+    }
+
+    @Test
+    public void givenWrongInput_shouldThrow() {
+        FactoryFloorGrid grid = FactoryFloorGrid.of("5 5");
+        CliState state = new CreateRobotState(grid);
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            state.input("abc");
+        });
+    }
+}
